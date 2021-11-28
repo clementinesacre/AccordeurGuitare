3ba525620dc1a4de890b3f1f2d5402e5041cb6bd
clementinesacre <74923610+clementinesacre@users.noreply.github.com>
2021-11-28 09:52:32 +0100

    Ajout de docstring et traduction en anglais des noms
    + correction du bug quand il arrive tout pile au bon endroit

--- interface.py
+++ interface.py
@@ -9,139 +9,129 @@ heightCanvas = 100
 freqRef = 0
 limit = 3 #1200/3 = 400hz ==> max
 
+"""
+Enregistrer en choisisant manuellement la note à atteindre
+"""
 def chooseNote(freq):
-    targetFreq = freq
     global limit
+    
     dict = main.getFrenquencies()
+    targetFreq = freq
     actualFrequency = dict["freqActu"]
+    print("frequence a atteindre manuellement : ", targetFreq)
+    print("frequence actuelle : ", actualFrequency)
+    print('-----------------------------')
     scale.changeScale(targetFreq)
     limit = widthCanvas/(targetFreq*2)
-    aiguille.changeTargetF(actualFrequency)
-    root.after_idle(Move)  # after_idle est appelé quand il n'y a plus d'événements à traiter dans la boucle principale.
-    #aiguille.move()
+    pointer.changeTargetF(actualFrequency)
+    root.after_idle(move) 
 
-
+"""
+Enregistrer en trouvant automatiquement la note à atteindre
+"""
 def startRecord():
     global limit
+    
     dict = main.getFrenquencies()
     targetFreq = dict["target_frequency"]
     actualFrequency = dict["freqActu"]
+    print("frequence a atteindre automatiquement : ", targetFreq)
+    print("frequence actuelle : ", actualFrequency)
+    print('-----------------------------')
     scale.changeScale(targetFreq)
     limit = widthCanvas/(targetFreq*2)
-    aiguille.changeTargetF(actualFrequency)
-    root.after_idle(Move)  # after_idle est appelé quand il n'y a plus d'événements à traiter dans la boucle principale.
-    #aiguille.move()
+    pointer.changeTargetF(actualFrequency)
+    root.after_idle(move) 
 
 
-def callback():
-    pass
+"""
+Faire bouger l'aiguille
+"""
+def move():
+    pointer.move()
+    root.after(10, move)
 
 
-def Move():
-    aiguille.move()
-    root.after(10, Move)
-
-"""def generateRand():
-    aiguille.random()
-    # aiguille.move()
-    root.after(2000, generateRand)"""
-
-
+"""
+Classe représentant l'échelle (avec les fréquences/tons affichés selon ce qui est choisi)
+"""
 class Scale:
     def __init__(self, canvas):
         self.canvas = canvas
         self.min = "A#"
         self.max = "e"
         self.center = 'G#'
-
         self.minCanvas = canvas.create_text(widthCanvas - 20, 20, text=self.min, fill="blue", font='Helvetica 15 bold')
         self.maxCanvas = canvas.create_text(10, 20, text=self.max, fill="blue", font='Helvetica 15 bold')
-        self.centerCanvas = canvas.create_text(widthCanvas / 2, 20, text=self.center, fill="blue",
-                                               font='Helvetica 15 bold')
+        self.centerCanvas = canvas.create_text(widthCanvas / 2, 20, text=self.center, fill="blue", font='Helvetica 15 bold')
 
     def changeScale(self, center):
         self.center = center
         self.canvas.itemconfig(self.centerCanvas, text=center)
 
-
-class Aiguille:
+"""
+Classe représentant l'aiguille qui se déplace pour afficher la bonne fréquence
+"""
+class Pointer:
     def __init__(self, canvas):
-        # self.master = master
-        # self.canvas = Canvas(master, bg="black", width=1200, height=100)
         self.canvas = canvas
         self.pos_x1 = widthCanvas/2
         self.pos_y1 = 50
         self.taille = 50
         self.target_freq = 0
         self.step = 5
-        self.rectangle = self.canvas.create_rectangle(self.pos_x1, self.pos_y1, self.pos_x1 + self.taille,
-                                                      self.pos_y1 + self.taille, fill="red",
-                                                      outline='blue')
+        self.rectangle = self.canvas.create_rectangle(self.pos_x1, self.pos_y1, self.pos_x1 + self.taille,self.pos_y1 + self.taille, fill="red", outline='blue')
         self.canvas.pack()
 
     def move(self):
-        # self.movement = self.pos_x1 - self.target_freq
         #print(" POS : " ,self.pos_x1, "  Limite : ",self.target_freq)
+        liste = []
+        for i in range(-self.step+1, self.step) :
+            liste.append(self.pos_x1 + i)
+
         if self.pos_x1 >= widthCanvas - self.taille or self.pos_x1 <= 0:
            pass
-        elif self.target_freq > self.pos_x1 or self.target_freq > self.pos_x1 + 4 or self.target_freq > self.pos_x1 - 4:
+        elif self.target_freq in liste :
+            #print("egal")
+            pass
+        elif self.target_freq > self.pos_x1 :
+            #print("plus grand")
             self.canvas.move(self.rectangle, self.step, 0)
             self.pos_x1 += self.step
-        elif self.target_freq < self.pos_x1 or self.target_freq < self.pos_x1 + 4 or self.target_freq < self.pos_x1 - 4:
+        elif self.target_freq < self.pos_x1 :
+            #print("plus petit")
             self.canvas.move(self.rectangle, -self.step, 0)
             self.pos_x1 -= self.step
-        elif self.target_freq == self.pos_x1:
-            pass
-
-        """elif self.target_freq * limit == self.pos_x1 or self.target_freq * limit <= self.pos_x1 - self.step or self.target_freq * limit >= self.pos_x1 + self.step:
-                    print("condition")"""
-
-
-        # self.pos_x1 = self.movement
 
     def changeTargetF(self, newF):
         self.target_freq = int(newF * limit)
 
-    """def random(self):
-        self.target_freq = randint(1, 1200)
-        print(self.target_freq)"""
-
 
 root = tk.Tk()
-root.title('Tkinter Window Demo')
+root.title('Accordeur guitare')
 root.geometry('1200x800+50+50')
 
-buttonEnregistrer = ttk.Button(root, text="enregistrer", command=lambda: startRecord())
-buttonEnregistrer.pack(ipadx=5, ipady=5, expand=True)
+buttonSave = ttk.Button(root, text="Enregistrer", command=lambda: startRecord())
+buttonSave.pack(ipadx=5, ipady=5, expand=True)
 
 buttonNote1 = ttk.Button(root, text="E", command=lambda: chooseNote(82.41))
 buttonNote1.pack(ipadx=5, ipady=5, expand=True)
-
-buttonNote2 = ttk.Button(root, text="a", command=lambda: chooseNote(110.00))
+buttonNote2 = ttk.Button(root, text="A", command=lambda: chooseNote(110.00))
 buttonNote2.pack(ipadx=5, ipady=5, expand=True)
-
-buttonNote3 = ttk.Button(root, text="d", command=lambda: chooseNote(146.83))
+buttonNote3 = ttk.Button(root, text="D", command=lambda: chooseNote(146.83))
 buttonNote3.pack(ipadx=5, ipady=5, expand=True)
-
-buttonNote4 = ttk.Button(root, text="g", command=lambda: chooseNote(196.00))
+buttonNote4 = ttk.Button(root, text="G", command=lambda: chooseNote(196.00))
 buttonNote4.pack(ipadx=5, ipady=5, expand=True)
-
-buttonNote5 = ttk.Button(root, text="b", command=lambda: chooseNote(246.93))
+buttonNote5 = ttk.Button(root, text="B", command=lambda: chooseNote(246.93))
 buttonNote5.pack(ipadx=5, ipady=5, expand=True)
-
-buttonNote6 = ttk.Button(root, text="e", command=lambda: chooseNote(329.63))
+buttonNote6 = ttk.Button(root, text="E", command=lambda: chooseNote(329.63))
 buttonNote6.pack(ipadx=5, ipady=5, expand=True)
 
 canvas = Canvas(master=root, bg="black", width=widthCanvas, height=heightCanvas)
+
 scale = Scale(canvas)
+pointer = Pointer(canvas)
 
-aiguille = Aiguille(canvas)
-#aiguille.move()
-
-
-# Appelé qu'une fois
-# root.after_idle(generateRand)
+# root.after_idle(generateRand) # after_idle est appelé quand il n'y a plus d'événements à traiter dans la boucle principale ; # Appelé qu'une fois
 
 root.mainloop()
-
-# imports every file form tkinter and tkinter.ttk
